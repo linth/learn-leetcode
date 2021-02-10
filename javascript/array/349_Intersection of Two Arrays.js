4929de1ea67cf2a2030482516649ed45ecedb719
linth <zhua0404@gmail.com>
2021-02-10 23:31:10 +0800

    solve the subject (349) for js.

--- javascript/array/349_Intersection of Two Arrays.js
+++ javascript/array/349_Intersection of Two Arrays.js
@@ -26,5 +26,31 @@ Reference:
  */
 
 var intersection = (nums1, nums2) => {
-    
+    let res = [];
+
+    // delete the duplicated number.
+    // nums1.sort();
+    // nums2.sort();
+
+    // console.log('nums1', nums1); // 排序不一定是穩定的.    
+    // console.log('nums2', nums2);
+
+    for (let i=0, max=nums1.length; i<max; i++) {
+        for (let j=0, max_=nums2.length; j<max_; j++) {
+            if (nums1[i] === nums2[j]) {
+                if (res.indexOf(nums1[i]) === -1) {
+                    res.push(nums1[i]);
+                }
+            }
+        }
+    }
+    return res;
 };
+
+
+var nums1 = [1, 2, 2, 1];
+var nums2 = [2, 2];
+var res = intersection(nums1, nums2);
+console.log('res', res);
+
+
