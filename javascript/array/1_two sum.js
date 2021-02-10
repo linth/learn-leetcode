9028dfdea6620191c95cd65b95f2f7c330da1f79
linth <zhua0404@gmail.com>
2021-02-10 14:42:51 +0800

    Create subject (1) for js.

---
+++ javascript/array/1_two sum.js
@@ -0,0 +1,41 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Reference:
+    - https://leetcode.com/problems/two-sum/
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+
+var twoSum = function(nums, target) {
+
+    for (let i=0, max=nums.length; i<max; i++) {
+        for (let j=i+1, max_=nums.length; j<max_; j++) {
+            if (nums[j] + nums[i] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+
+var nums = [3, 2, 4];
+var target = 6;
+var result = twoSum(nums, target);
+console.log('result: ', result);
+
+
