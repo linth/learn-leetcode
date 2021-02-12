ac7c2d3397a441a5d7ce409627ba7fbda0eb8b5e
linth <zhua0404@gmail.com>
2021-02-13 01:51:12 +0800

    create subject (5) for python.

---
+++ python/string/5_Longest Palindromic Substring.py
@@ -0,0 +1,28 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: s = "cbbd"
+Output: "bb"
+
+Reference:
+    - https://leetcode.com/problems/longest-palindromic-substring/
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    st = "babad"
+    s = Solution()
+    res = s.longestPalindrome(st)
+    print('res', res)
+
