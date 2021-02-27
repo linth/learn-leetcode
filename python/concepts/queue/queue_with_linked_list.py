89bb7107ed8e494de79dbdf7abd5241e2b73ae39
linth <zhua0404@gmail.com>
2021-02-27 16:06:42 +0800

    add traverse_list function for queue with linked list.

--- python/concepts/queue/queue_with_linked_list.py
+++ python/concepts/queue/queue_with_linked_list.py
@@ -22,15 +22,16 @@ class Queue:
     def is_empty(self) -> bool:
         return self.front == None
 
-    def enqueue(self, item) -> None:
+    def enqueue(self, item):
         if not isinstance(item, Node):
             temp = Node(item)
 
         if self.rear == None:
             self.front = self.rear = temp
-            return
+            return self
         self.rear.next = temp
         self.rear = temp
+        return self
 
     def dequeue(self) -> None:
         if self.is_empty():
@@ -42,6 +43,16 @@ class Queue:
         if self.front is None:
             self.rear = None
 
+    def traverse_list(self):
+        temp = self.front
+
+        l = []
+        while temp is not None:
+            print(temp.data)
+            l.append(temp.data)
+            temp = temp.next
+        return l
+
 
 if __name__ == '__main__':
     q = Queue()
@@ -51,3 +62,6 @@ if __name__ == '__main__':
     q.dequeue()
     q.enqueue(2)
     print(q.front.data, q.rear.data)
+
+    res = q.traverse_list()
+    print('the result = ', res)
