0b702077cf90be048251d6641786daa3ebc951bd
linth <zhua0404@gmail.com>
2021-03-26 20:20:33 +0800

    create the subject (535).

---
+++ python/hashtable/535_Encode and Decode TinyURL.py
@@ -0,0 +1,33 @@
+"""
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as 
+https://leetcode.com/problems/design-tinyurl and it returns a short URL such as 
+http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. 
+There is no restriction on how your encode/decode algorithm should work. 
+You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+
+Reference 
+    - https://leetcode.com/problems/encode-and-decode-tinyurl/
+"""
+
+class Codec:
+    
+    def encode(self, longUrl: str):
+        # encodes a URL to shortened URL.
+        pass
+
+    def decode(self, shortUrl: str):
+        # decodes a shortened URL to its original URL.
+        pass
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+
+
+if __name__ == '__main__':
+    pass
+
