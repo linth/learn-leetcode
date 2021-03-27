56036425cb16152d932833f2344bd5880d876f42
linth <zhua0404@gmail.com>
2021-03-27 20:43:05 +0800

    solve the subject (535).

--- python/hashtable/535_Encode and Decode TinyURL.py
+++ python/hashtable/535_Encode and Decode TinyURL.py
@@ -12,16 +12,46 @@ You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL
 Reference 
     - https://leetcode.com/problems/encode-and-decode-tinyurl/
 """
+import string
+import random 
+import math
+
+
+full_tiny, tiny_full = {}, {}
+letters = string.ascii_letters + string.digits
+
 
 class Codec:
-    
+
+    # Runtime: 36 ms, faster than 43.51%, Memory Usage: 14.4 MB, less than 30.45%
+    """ 較佳做法 """    
     def encode(self, longUrl: str):
         # encodes a URL to shortened URL.
-        pass
+
+        def short_add():
+            ans = ''
+            tmp = ''
+            for i in range(6):
+                tmp = letters[random.randint(0, 10000) % 62]
+                ans += tmp
+            return ans
+
+        if longUrl in full_tiny:
+            return 'http://tinyurl.com/' + full_tiny[longUrl]
+        else:
+            suffix = short_add()
+            full_tiny[longUrl] = suffix
+            tiny_full[suffix] = longUrl
+            return 'http://tinyurl.com/' + suffix
+
 
     def decode(self, shortUrl: str):
         # decodes a shortened URL to its original URL.
-        pass
+        shortUrl = shortUrl.split('/')[-1]
+        if shortUrl in tiny_full:
+            return tiny_full[shortUrl]
+        else:
+            return None
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
@@ -29,5 +59,12 @@ class Codec:
 
 
 if __name__ == '__main__':
-    pass
+    url = 'http://www.google.com/ssssdawf'
+    codec = Codec()
+
+    shortUrl = codec.encode(url)
+    print('shortUrl: ', shortUrl)
+
+    orignUrl = codec.decode(shortUrl)
+    print('orignUrl', orignUrl)
 
