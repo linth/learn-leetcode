e24fdd1bf25945dcea3bd86ddc8617b3a492dc9c
linth <zhua0404@gmail.com>
2021-03-27 17:40:59 +0800

    solve the subject (85).

--- python/hashtable/85_Maximal Rectangle.py
+++ python/hashtable/85_Maximal Rectangle.py
@@ -33,12 +33,55 @@ Constraints:
 Reference
     - https://leetcode.com/problems/maximal-rectangle/
 """
+from typing import List
 
 
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]):
-        pass
+        if not matrix: 
+            return 0
+
+        if not matrix:
+            return 0
+        res = 0
+        height = [0] * len(matrix[0])
+        for row in matrix:
+            for i in range(len(matrix[0])):
+                if row[i] == '0':
+                    height[i] = 0
+                else:
+                    height[i] += 1
+            res = max(res, self.maxRect(height))
+        return res
+
+    def maxRect(self, height):
+        res = 0
+        stack = []
+        height.append(0)
+        for i in range(len(height)):
+            if not stack or height[i] > height[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and height[stack[-1]] > height[i]:
+                    w = height[stack[-1]]
+                    stack.pop()
+                    h = i if not stack else i - stack[-1] - 1
+                    res = max(res, w * h)
+                stack.append(i)
+        return res
 
 
 if __name__ == '__main__':
-    pass
+    
+    matrix = [
+        ["1","0","1","0","0"],
+        ["1","0","1","1","1"],
+        ["1","1","1","1","1"],
+        ["1","0","0","1","0"],
+    ]
+
+    matrix2 = []
+
+    s = Solution()
+    res = s.maximalRectangle(matrix2)
+    print('res', res)
