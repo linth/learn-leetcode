807a8e888f1b6546940066dd669fafaa45a4ec1f
linth <zhua0404@gmail.com>
2022-06-28 10:33:25 +0800

    add code to let example completely.

--- python/concepts/tree/Inorder.py
+++ python/concepts/tree/Inorder.py
@@ -1,12 +1,13 @@
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 class Solution:
-    def inorderTraversal(self, root: TreeNode):
+    def inorderTraversal(self, root):
         if not root:
             return []
         
@@ -25,5 +26,13 @@ class Solution:
 
 
 if __name__ == '__main__':
-    pass
+    
+    node3 = TreeNode(3)
+    node2 = TreeNode(2, TreeNode(4), TreeNode(5))
+    node1 = TreeNode(1, node2, node3)
+    
+    s = Solution()
+    res = s.inorderTraversal(node1)
+    print('res', res)
+    # write some unit test code.
     
