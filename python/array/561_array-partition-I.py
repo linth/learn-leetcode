aa38cb82a07b4186807ba0cddb56b082be6fb194
linth <zhua0404@gmail.com>
2022-07-09 01:38:35 +0800

    add 561 leetcode (not finish it.)

---
+++ python/array/561_array-partition-I.py
@@ -0,0 +1,19 @@
+"""
+Given an array of 2n integers, your task is to group these integers into n pairs of integer,
+ say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example:
+    Input: [1,4,3,2]
+
+    Output: 4
+    Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+"""
+
+
+class Solution:
+    def arrayPairSum(self, nums) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    pass
