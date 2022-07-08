f2986f797871cc0ae3035dea75255fcb0cebbac9
linth <zhua0404@gmail.com>
2022-07-09 01:38:53 +0800

    add 697 leetcode.

---
+++ python/array/697_Degree-of-an-Array.py
@@ -0,0 +1,43 @@
+"""
+No. 697. Degree of an Array
+
+Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+
+Example 1:
+    Input: [1, 2, 2, 3, 1]
+    Output: 2
+    Explanation:
+    The input array has a degree of 2 because both elements 1 and 2 appear twice.
+    Of the subarrays that have the same degree:
+    [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+The shortest length is 2. So return 2.
+
+Example 2:
+    Input: [1,2,2,3,1,4,2]
+    Output: 6
+
+Constraints:
+    - nums.length will be between 1 and 50,000.
+    - nums[i] will be an integer between 0 and 49,999.
+
+Reference
+    - https://leetcode.com/problems/degree-of-an-array/
+"""
+
+
+class Solution(object):
+    def findShortestSubArray(self, nums):
+        left, right, count = {}, {}, {}
+        for i, x in enumerate(nums):
+            if x not in left: left[x] = i
+            right[x] = i
+            count[x] = count.get(x, 0) + 1
+
+        ans = len(nums)
+        degree = max(count.values())
+        for x in count:
+            if count[x] == degree:
+                ans = min(ans, right[x] - left[x] + 1)
+
+        return ans
